59dd09c148677310650151f4cfeb0595726a155f
ahmadkabi <achmad.kabi@ideku.id>
2023-09-07 06:59:52 +0700

    add view binding

--- app/src/main/java/com/kodingan/uicollection/HomeActivity.kt
+++ app/src/main/java/com/kodingan/uicollection/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.kodingan.uicollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodingan.uicollection.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
