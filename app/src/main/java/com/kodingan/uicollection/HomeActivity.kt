bf0b62d1c4a9abe3ddbbc287a727e54a50d9bd8f
ahmadkabi <qolamunapp@gmail.com>
2024-01-27 16:35:42 +0700

    Liked text

--- app/src/main/java/com/kodingan/uicollection/HomeActivity.kt
+++ app/src/main/java/com/kodingan/uicollection/HomeActivity.kt
@@ -1,6 +1,11 @@
 package com.kodingan.uicollection
 
+import android.graphics.Typeface.BOLD
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.kodingan.uicollection.databinding.ActivityHomeBinding
 
@@ -13,5 +18,28 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        buildLikedText()
     }
+
+    private fun buildLikedText() {
+        val builder = SpannableStringBuilder("Liked by ")
+
+        val omameSpannable = SpannableString("omame_munchkin ")
+        val othersSpannable = SpannableString("2.462 others")
+
+        omameSpannable.setSpan(
+            StyleSpan(BOLD), 0, omameSpannable.length, 0
+        )
+        othersSpannable.setSpan(
+            StyleSpan(BOLD), 0, othersSpannable.length, 0
+        )
+
+        builder.append(omameSpannable)
+        builder.append("and ")
+        builder.append(othersSpannable)
+
+        binding.txLiked.setText(builder, TextView.BufferType.SPANNABLE)
+
+    }
+
 }
